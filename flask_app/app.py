9650cf7f8220d079ea5c2b0c5621c61b01d95004
JesusRamirezGamarra <luciojesusramirezgamarra@gmail.com>
2025-02-10 23:26:37 -0500

    first commit - Change Deploy.yaml ---- Passphrase

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,12 +1,13 @@
-from flask import Flask
 import os
+
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     container_id = os.uname()[1]
-    return f"hello world {container_id}"
+    return f"hello world {container_id} --- LucioJesusRamirezGamarra"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
